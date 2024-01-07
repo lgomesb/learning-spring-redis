51cb50a27fd6f19f9ecd7d1e2d3fea9ed261e552
leandro <lgomesb@gmail.com>
2024-01-07 23:58:01 +0000

    Example 2 Spring cache with spring

--- src/main/java/pt/com/learning/spring/redis/learningspringredis/LearningSpringRedisApplication.java
+++ src/main/java/pt/com/learning/spring/redis/learningspringredis/LearningSpringRedisApplication.java
@@ -1,47 +1,66 @@
 package pt.com.learning.spring.redis.learningspringredis;
 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
-import redis.clients.jedis.Jedis;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 
-
+@EnableCaching
 @SpringBootApplication
-public class LearningSpringRedisApplication implements CommandLineRunner {
-
-
+public class LearningSpringRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringRedisApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Jedis jedis = new Jedis("localhost", 6379);
-		jedis.auth("redis-stack");
-		System.out.println("Connected to Redis");
-
-		String key = UUID.randomUUID().toString();
-        // System.out.println("Valor: " + key);
-		// String value = "Hello Redis";
-		// jedis.set(key, value);
-		// String result = jedis.get(key);
-		Map<String,String> map = new HashMap<>();
-		map.put("firstName", "Fulano");
-		map.put("lastname", "Silva");
-		map.put("age", "45");
-		map.put("gender", "MALE");
-		String result = jedis.hmset(key, map);
-        System.out.println("Valor: " + result);
-
-		jedis.close();
+	@Bean
+	ApplicationRunner runner(ProductService productService) {
+		return args -> {
+			System.out.println(System.lineSeparator());
+			System.out.println("Id: 1 " + productService.getById(1L));
+			System.out.println("Id: 2 " + productService.getById(2L));
+			System.out.println("Id: 1 " + productService.getById(1L));
+			System.out.println("Id: 1 " + productService.getById(1L));
+			System.out.println("Id: 1 " + productService.getById(1L));
+		};
 	}
+	record Product(Long id, String name, String description) implements Serializable {}
 
+	@Service
+	class ProductService {
+
+		Map<Long, Product> products = new HashMap<>() {
+			{
+				put(1L, new Product(1L, "Laptop", "Macbook Air"));
+				put(2L, new Product(2L, "Laptop", "HP"));
+				put(3L, new Product(3L, "Laptop", "XPS"));
+				put(4L, new Product(4L, "Laptop", "Thinkpad"));
+				put(5L, new Product(5L, "Laptop", "Chromebook"));
+			}
+		};
+
+		@Cacheable("products")
+		Product getById(Long id) {
+			System.out.println("Getting products ...");
+			latencySimulation();
+			return products.get(id);
+		}
+
+		private void latencySimulation() {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+	}
 }
